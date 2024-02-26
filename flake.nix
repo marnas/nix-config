cbf78923dfc4109d3c95617310339d654ca87698
Marco Santonastaso <marco@santonastaso.com>
2024-02-26 11:54:44 +0000

    folder restructure initialised

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
+
     split-monitor-workspaces = {
       url = "github:Duckonaut/split-monitor-workspaces";
       inputs.hyprland.follows = "hyprland";
@@ -35,20 +37,41 @@
 
     in
     {
-
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
-        nixos = lib.nixosSystem {
-          inherit system;
-          modules = [ ./configuration.nix ];
+        nixos = nixpkgs.lib.nixosSystem {
+          # specialArgs = { inherit inputs outputs; };
+          modules = [
+            # > main nixos configuration file <
+            ./nixos/configuration.nix
+          ];
         };
       };
+
       homeConfigurations = {
         # useGlobalPkgs = true;
         # useUserPackages = true;
         marnas = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
-            ./home.nix
+            ./home-manager/home.nix
+          ];
+        };
+      };
+
+
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        "marnas@nixos" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          # pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          # extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # > main home-manager configuration file <
+            ./home-manager/home.nix
+
             hyprland.homeManagerModules.default
             {
               wayland.windowManager.hyprland = {
@@ -61,6 +84,6 @@
           ];
         };
       };
-    };
 
+    };
 }
