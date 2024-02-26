35613c12902ea31740aad1333c4ebe53195a94ca
Marco Santonastaso <marco@santonastaso.com>
2024-02-26 16:31:57 +0000

    old postman overlay commented out

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,15 +10,15 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    postman = prev.postman.overrideAttrs (old: rec {
-      version = "20230716100528";
-      src = final.fetchurl {
-        url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
-        sha256 = "sha256-svk60K4pZh0qRdx9+5OUTu0xgGXMhqvQTGTcmqBOMq8=";
-
-        name = "${old.pname}-${version}.tar.gz";
-      };
-    });
+    # postman = prev.postman.overrideAttrs (old: rec {
+    #   version = "20230716100528";
+    #   src = final.fetchurl {
+    #     url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
+    #     sha256 = "sha256-svk60K4pZh0qRdx9+5OUTu0xgGXMhqvQTGTcmqBOMq8=";
+    #
+    #     name = "${old.pname}-${version}.tar.gz";
+    #   };
+    # });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
