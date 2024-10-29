09713a491c304cd20f1878572028bba0dc5b5d7b
Marco Santonastaso <marco@santonastaso.com>
2024-10-29 15:52:26 +0000

    atuin config updated

--- home-manager/cli/atuin.nix
+++ home-manager/cli/atuin.nix
@@ -2,5 +2,14 @@
   programs.atuin = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://atuin.marnas.sh";
+      filter_mode = "global";
+      filter_mode_shell_up_key_binding = "directory";
+      search_mode_shell_up_key_binding = "prefix";
+      enter_accept = true;
+    };
   };
 }
