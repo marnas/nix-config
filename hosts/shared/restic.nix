b633973bd255096af580d2f5ed6e21eb8d844ce0
Marco Santonastaso <marco@santonastaso.com>
2024-11-26 09:34:27 +0000

    restic config updated

--- hosts/shared/restic.nix
+++ hosts/shared/restic.nix
@@ -1,25 +1,37 @@
 let
   user = "marnas";
-  backupRepo = "/mnt/backup/home";
+  backupRepo = "/mnt/backup";
   envFile = "/secrets/s3_env";
   passwordFile = "/secrets/restic";
   repositoryFile = "/secrets/repository";
-in { ... }: {
+  homeBackup = [
+    "/home/${user}/Desktop/"
+    "/home/${user}/Documents/"
+    "/home/${user}/Music/"
+    "/home/${user}/Pictures/"
+    "/home/${user}/Templates/"
+    "/home/${user}/Videos/"
+  ];
+in { pkgs, ... }: {
   services.restic.backups = {
     homebackup = {
       user = "${user}";
       exclude = [ ".git/" ];
       initialize = true;
       passwordFile = "${passwordFile}";
-      paths = [
-        "/home/${user}/Desktop/"
-        "/home/${user}/Documents/"
-        "/home/${user}/Music/"
-        "/home/${user}/Pictures/"
-        "/home/${user}/Templates/"
-        "/home/${user}/Videos/"
-      ];
       repository = "${backupRepo}";
+      paths = homeBackup;
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 2"
+        "--keep-yearly 0"
+      ];
+      extraBackupArgs = [ "--exclude-caches" ];
+      timerConfig = {
+        OnCalendar = "daily"; # Empty string to disable the timer
+        Persistent = true;
+      };
     };
     s3backup = {
       initialize = true;
@@ -33,23 +45,15 @@ in { ... }: {
         "--keep-monthly 2"
         "--keep-yearly 0"
       ];
-      paths = [
-        "/home/${user}/Desktop/"
-        "/home/${user}/Documents/"
-        "/home/${user}/Music/"
-        "/home/${user}/Pictures/"
-        "/home/${user}/Templates/"
-        "/home/${user}/Videos/"
-      ];
-      # backupPrepareCommand =
-      #   "${pkgs.restic}/bin/restic unlock"; # necessary to prevent locks from persisting indefinitely. See more:
+      paths = homeBackup;
+      backupPrepareCommand =
+        "${pkgs.restic}/bin/restic unlock"; # necessary to prevent locks from persisting indefinitely. See more:
       # https://forum.restic.net/t/restic-unlock-automation/5511
       extraBackupArgs = [ "--exclude-caches" ];
       timerConfig = {
-        # OnCalendar = "hourly"; # Empty string to disable the timer
+        OnCalendar = "daily"; # Empty string to disable the timer
         Persistent = true;
       };
-
     };
   };
 }
