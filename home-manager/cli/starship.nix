a19c0b3564ff09eb9458fc4bd7ada81353110358
Marco Santonastaso <marco@santonastaso.com>
2025-05-09 17:06:00 +0100

    starship config and nix flake updated

--- home-manager/cli/starship.nix
+++ home-manager/cli/starship.nix
@@ -1,6 +1,43 @@
-{ ... }: {
+{ lib, ... }: {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_status"
+        "$aws"
+        "$kubernetes"
+        "$line_break"
+        "$status"
+        "$shell"
+        "$character"
+      ];
+      kubernetes = {
+        disabled = false;
+        format = "[$symbol$context( )]($style) ";
+        symbol = " ";
+        style = "bold blue";
+        contexts = [
+          {
+            context_pattern = "cdk-eks-prod";
+            style = "red bold";
+          }
+          {
+            context_pattern = "cdk-gke-prod";
+            style = "red bold";
+          }
+        ];
+      };
+      aws = {
+        disabled = false;
+        format = "[$symbol$profile]($style) ";
+        style = "bold yellow";
+        symbol = "󰅟 ";
+      };
+    };
   };
 }
